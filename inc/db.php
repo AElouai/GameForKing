6ecf0e130722016369ecfc7c721f3d4e32d65b41
ALI ELOUAI <happy-ali@live.fr>
2015-03-11 08:08:32 +0000

    new start

--- inc/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('./inc/DB.class.php');
-$db=DB::start();
-if($db =="false"){
-die("Database connection Failed : ".mysqli_connect_error()."(".mysqli_connect_errno().")");
-}
-
-?>
