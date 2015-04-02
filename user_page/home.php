830ff9ade3c1217049b8028f8fcf9d4674659549
haikei00X <douae.haddad@gmail.com>
2015-04-02 03:29:27 +0100

    User dashboard (Don't pay attention to the content)

---
+++ user_page/home.php
@@ -0,0 +1,237 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Dashboard</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Flatro Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/animate.css" rel="stylesheet" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<script>$(document).ready(function(c) {
+	$('.alert-close').on('click', function(c){
+		$('.message').fadeOut('slow', function(c){
+	  		$('.message').remove();
+		});
+	});	  
+});
+</script>
+<script>$(document).ready(function(c) {
+	$('.alert-close1').on('click', function(c){
+		$('.message1').fadeOut('slow', function(c){
+	  		$('.message1').remove();
+		});
+	});	  
+});
+</script>
+<script type="text/javascript" src="js/wow.min.js"></script></p>
+<script type="text/javascript">
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    }
+  }
+);
+wow.init();
+</script>
+</head>
+<body>
+<div class="header_top">
+	<div class="container">
+	
+	
+         <div class="cssmenu">
+			<ul>
+			    <li class="active"><a href="logout.html">Logout</a></li> 
+			</ul>
+		 </div>
+	</div>
+</div>	
+<div class="wrap-box"></div>
+<div class="header_bottom">
+	    <div class="container">
+			<div class="col-xs-9 header-bottom-left">
+				<div class="col-xs-2 logo">
+					<h1><a href="index.html"><span>Our</span>Logo</a></h1>
+				</div>
+				<div class="col-xs-7 menu">
+		            <ul class="megamenu skyblue">
+				      <li class="active grid"><a class="color1" href="index.html">We</a><div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+							
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+					
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+							
+						
+								</div>												
+							</div>
+						  </div>
+						</div>
+					</li>
+				    <li class="grid"><a class="color2" href="#">Can</a>
+					  <div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+						
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+						
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+				
+						
+								</div>												
+							</div>
+						  </div>
+						</div>
+			    </li>
+				<li><a class="color4" href="about.html">Put</a></li>				
+				<li><a class="color5" href="404.html">Something</a></li>
+				<li><a class="color6" href="contact.html">Here</a></li>
+			  </ul> 
+			</div>
+		</div>
+	   
+	                
+                            <div class="" >
+							  <div class="header-nav-link ">
+							 
+							  <b><a href="profile.html">User:   Unicorn Freak</a></b>
+							  </div>
+							  <div class="clearfix"></div>
+						    </div>
+					      <div class="clearfix"></div>
+						</ul>
+					 </li>
+		      </ul>
+              
+             <div class="clearfix"></div>
+          </div>
+        <div class="clearfix"></div>
+	 </div>
+</div>
+<div class="banner">
+	<div class="container">
+		<div class="banner_desc">
+			<h1>Welcome to GamingLAnd.</h1>
+			<h2>Choose a game and start the adventure...</h2>
+			<div class="button">
+			      <a href="random.php" class="hvr-shutter-out-horizontal">Play Now</a>
+			    </div>
+		</div>
+	</div>
+</div>	
+
+
+<div class="content_bottom">
+<div class="container">
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow slideInLeft" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="maths.php"><img src="images/maths.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow flip" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="physics.php"><img src="images/physics.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+  	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow bounceIn" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="music.php"><img src="images/music.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow swing" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="knowledge.php"><img src="images/knowledge.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow wobble" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="cinema.php"><img src="images/cinema.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow tada" data-wow-duration="2s" data-wow-delay="1s">
+		         <a href="history.php"><img src="images/history.jpg"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	 
+	
+	</div>
+			</div>   
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>	
+</div>
+<div class="content_bottom-grid">
+	
+	</div>
+	<div class="clearfix"> </div>
+</div>
+
+</body>
+</html>		
