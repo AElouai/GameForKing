317046ab47776658506f9e571c1deaa7ab1ef1a5
Amine Hakkou <amine.hakkou.pro@gmail.com>
2015-04-04 16:20:42 +0000

    styled /profile

---
+++ php/profile.php
@@ -0,0 +1,33 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-3 col-sm-6">
+            <div class="card hovercard">
+                <div class="cardheader"></div>
+                <div class="avatar">
+                    <img alt="" src="/profile-pictures/default.jpg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a href="http://scripteden.com/">G4K guy</a>
+                    </div>
+                    <div class="desc">Stats</div>
+                    <div class="desc">More stats...</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="#">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" href="#">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" href="#">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+      </div>
+			<div class="col-lg-9 col-sm-6">
+				in here we'll have his wall,posts and stuff
+			</div>
+	</div>
+</div>
