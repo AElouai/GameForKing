e01ece9005cc4324463a58f29060204936197b3e
Amine Hakkou <amine.hakkou.pro@gmail.com>
2015-04-04 01:59:07 +0000

    started styling /game

--- php/game.php
+++ php/game.php
@@ -1,4 +1,16 @@
 <?php
 if(!isset($isIndex))die('');
 ?>
-in here you'll be able to play.
+<div class="row">
+  <div class="col-xs-12 col-md-2 player" id="player1">
+    <img src="/profile-pictures/default.jpg">
+    <p>Gameforking guy</p>
+  </div>
+  <div class="col-xs-12 col-md-8" id="gameContainer">
+
+  </div>
+  <div class="col-xs-12 col-md-2 player" id="player2">
+    <img src="/profile-pictures/default.jpg">
+    <p>Somebody else</p>
+  </div>
+</div>
