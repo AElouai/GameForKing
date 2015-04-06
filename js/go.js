17a0a839ed1bea7bb93689c095c90329b719c530
Amine Hakkou <amine.hakkou.pro@gmail.com>
2015-04-06 23:47:12 +0000

    added random coloring for /go tiles :D

--- js/go.js
+++ js/go.js
@@ -14,12 +14,19 @@ $( document ).ready(function() {
     $(window).bind('resizeEnd', function() {
     	$(".play-btn").height($("#tile1").width());
     	$(".tile").height($("#tile1").width());
-      $(".carousel").height($("#tile1").width());
-      $(".item").height($("#tile1").width());
+        $(".carousel").height($("#tile1").width());
+        $(".item").height($("#tile1").width());
     });
 
     $('.tile').click(function(){
       alert('i am clicked');
       $(this).addClass('tile-selected');//just for now it only changes color,next up it should show checked mark
+    });
+    //generating random color for each tile
+    $('.tile').each(function(){
+        var tile = $(this);
+        var colors = ['00ACEE','C0C0C0','47C1E4','D77F3C','AF1A3F','3E9DD7','4E1616','D14625','008E00','005DE9'];
+        var randValue = Math.floor(Math.random() * colors.length);
+        tile.css('background-color',colors[randValue]);
     });
 });
