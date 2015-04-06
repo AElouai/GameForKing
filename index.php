c608f3b65dab8ec6d757fe0f8653347b99233c2b
Amine Hakkou <amine.hakkou.pro@gmail.com>
2015-04-06 21:51:45 +0000

    added .gitignore, added a favicon

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="/img/icon.png">
     <title>Game For King</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
