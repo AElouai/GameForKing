5bdab007d570db50a58ac5178b456854f78bb264
ALI ELOUAI <happy-ali@live.fr>
2015-04-07 00:15:26 +0000

    Merge

--- php/home.php
+++ php/home.php
@@ -1,5 +1,13 @@
-hello world!
+<div claas="container">
+	<div id="home">
+		<p class="title">Entrée le Mode des batailles des connaissances et reflections et regnier sur le royaume
+</p>
+		<
+	</div>
 
+
+
+</div>
 <?php
 //setAlert('success','hey..ho, we are on the way..check php/home.php to see how easy it is to use our alert system.');// this one is here to show the muscles of our alert system :D
 ?>
