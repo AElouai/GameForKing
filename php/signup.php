8d77f7917191749d76d4e52c33eaa376f804cbf2
ALI ELOUAI <happy-ali@live.fr>
2015-04-08 18:23:22 +0000

    nothing here to see

--- php/signup.php
+++ php/signup.php
@@ -20,11 +20,11 @@ if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password
   }
   if(User::signup(Array('email'=>$email,'password'=>$password,'link'=>$link))){
     setAlert('success','hello '.User::getFullName().', Welcome aboard!');
-    //redirect('/game');
+    redirect('/game');
   }
   else{
     setAlert('danger','could not connect with the given credentials.');
-   // redirect('/home');
+    redirect('/home');
   }
 }
 else{
