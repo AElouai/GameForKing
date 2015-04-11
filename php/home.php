31c0a0b7c5925a920be5ea8a92e68c193025041a
haikei00X <douae.haddad@gmail.com>
2015-04-11 02:14:02 +0100

    user page and profile pictures

--- php/home.php
+++ php/home.php
@@ -1,14 +1,347 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Dashboard</title>
+<link href="/user_page_pictures/css/button.css" rel="stylesheet" />
+     <audio preload="auto">
+       <source src="https://github.com/nclud/2011.beercamp.com/blob/gh-pages/audio/inception.mp3?raw=true" type="audio/mp3" />
+       <source src="https://github.com/nclud/2011.beercamp.com/blob/gh-pages/audio/inception.ogg?raw=true" type="audio/ogg" />
+     </audio>
+	 <script language="javascript" >
+	  function sound() {
+    document.getElementsByTagName('audio')[0].play();
+    document.getElementsByTagName('span')[0].innerHTML = 'Have Fun!';
+    return false;
+    }
+	 </script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Flatro Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="/user_page_pictures/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Custom Theme files -->
+<link href="/user_page_pictures/css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<!-- start menu -->
+<link href="/user_page_pictures/css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/user_page_pictures/css/animate.css" rel="stylesheet" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<script>$(document).ready(function(c) {
+	$('.alert-close').on('click', function(c){
+		$('.message').fadeOut('slow', function(c){
+	  		$('.message').remove();
+		});
+	});	  
+});
+</script>
+<script>$(document).ready(function(c) {
+	$('.alert-close1').on('click', function(c){
+		$('.message1').fadeOut('slow', function(c){
+	  		$('.message1').remove();
+		});
+	});	  
+});
+</script>
+<script type="text/javascript" src="js/wow.min.js"></script></p>
+<script type="text/javascript">
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    }
+  }
+);
+wow.init();
+</script>
+</head>
+<body>
+<div class="header_top">
+	<div class="container">
+	
+	
+         <div class="cssmenu">
+			<ul>
+			    <li class="active"><a href="index.php"><img src="images/logout.png"></a></li> 
+			</ul>
+		 </div>
+	</div>
+</div>	
+<div class="wrap-box"></div>
+<div class="header_bottom">
+	    <div class="container">
+			<div class="col-xs-9 header-bottom-left">
+				<div class="col-xs-2 logo">
+					<h1><img src="images/gflogo.jpg"></h1>
+				</div>
+				<div class="col-xs-7 menu">
+		            <ul class="megamenu skyblue">
+				      <li class="active grid"><a class="color1" href="index.html">We</a><div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+							
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+					
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+							
+						
+								</div>												
+							</div>
+						  </div>
+						</div>
+					</li>
+				    <li class="grid"><a class="color2" href="#">Can</a>
+					  <div class="megapanel">
+						<div class="row">
+							<div class="col1">
+								<div class="h_nav">
+						
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+						
+								</div>							
+							</div>
+							<div class="col1">
+								<div class="h_nav">
+				
+						
+								</div>												
+							</div>
+						  </div>
+						</div>
+			    </li>
+				<li><a class="color4" href="about.html">Put</a></li>				
+				<li><a class="color5" href="404.html">Something</a></li>
+				<li><a class="color6" href="contact.html">Here</a></li>
+			  </ul> 
+			</div>
+		</div>
+	   
+	                
+                            <div class="" >
+							  <div class="header-nav-link ">
+							    <img src="img/default-44.jpg">
+							  <b><a href="profile.php" >haikei00X</a></b>
+							  </div>
+							  <div class="clearfix"></div>
+						    </div>
+					      <div class="clearfix"></div>
+						</ul>
+					 </li>
+		      </ul>
+              
+             <div class="clearfix"></div>
+          </div>
+        <div class="clearfix"></div>
+	 </div>
+</div>
+<div class="banner">
+	<div class="container">
+		<div class="banner_desc">
+			<h1>Welcome to GamingForking.</h1>
+			<h2>Choose a game and start the adventure...</h2>
+		
+		</div>
+	</div>
+</div>	
+
+
+<div class="content_bottom">
+<div class="container">
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow swing" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="biology.php"><img src="images/biology.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow flip" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="chemistry.php"><img src="images/chemistry.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+  	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow bounceIn" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="history.php"><img src="images/history.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow swing" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="art.php"><img src="images/art.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow wobble" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="music.php"><img src="images/music.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>
+	<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow tada" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="geography.php"><img src="images/geography.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	 
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow rollIn" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="languages.php"><img src="images/languages.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow slideOutDown" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="literature.php"><img src="images/literature.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow rotateIn" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="math.php"><img src="images/math.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow bounceIn" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="politics.php"><img src="images/politics.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow pulse" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="technology.php"><img src="images/technology.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow swing" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="fitness.php"><img src="images/fitness.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+	    <div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+		    <div class="shop-holder">
+	           <section class="wow flip" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="physics.php"><img src="images/physics.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+	    <div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+		  <div class="shop-holder">
+	           <section class="wow flip" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="religion.php"><img src="images/religion.png"></a>
+		       </section>
+	       </div>
+	    </div>
+	</div>	
+		<div class="section group">
+		<div class="col_1_of_3 span_1_of_3">
+			<div class="shop-holder">
+	           <section class="wow rubberBand" data-wow-duration="5s" data-wow-delay="1s">
+		         <a href="problems.php"><img src="images/problems.png"></a>
+		       </section>
+	       </div>
+	    </div>
+		  	<div class="button">
+				 <section class="wow swing" data-wow-duration="2s" data-wow-delay="3s"> 
+			      <center><a href="#" id="button" class="hvr-shutter-out-horizontal" onclick="javascript:sound();"><span>Play Now!</span></a></center>
+				   </section>
+			    </div>
+	</div>	
+             
+	</div>
+	
+            	
+			</div>   
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>	
+</div>
+<div class="content_bottom-grid">
+	
+	</div>
+	<div class="clearfix"> </div>
+</div>
+
 
 	<div id="home">
 		<div class="row center-block">
 			<div class="col-md-12">
-				<h4 class="text-center">Entrée le Monde des batailles des connaissances et reflections et regnier sur le royaume</h4>
+				<h2 class="text-center">Entrez le Monde de batailles, de connaissances et de réflections. Reigniez sur le royaume!!</h2>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-2"></div>
 			<div class="col-md-8">
-				<img class="img-responsive" src="/img/home1.png">
+				<img class="img-responsive" src="images/home1.png">
 			</div>
 			<div class="col-md-2"></div>
 		</div>
@@ -18,13 +351,13 @@
 		<div class="row center-block">
 			<div class="col-md-2"></div>
 			<div class="col-md-4">
-				<h2 class="text-center">Défier le monde pour la couronne</h2>
+				<h2 class="text-center">Défiez le monde pour la couronne</h2>
 				<p class="text-center">
 					challenge opponents from across the globe and engage in real-time brain battles. As your level increases your opponents will get stronger. Prove that you deserve to be the KING.
 				</p>
 			</div>
 			<div class="col-md-4">
-				<img class="img-responsive img-circle center-block" src="/img/kingo.png">
+				<img class="img-responsive img-circle center-block" src="images/kingo.png">
 			</div>
 			<div class="col-md-2"></div>
 		</div>
@@ -33,19 +366,18 @@
 			<div class="col-md-2"></div>
 			<div class="col-md-2 pull-right"></div>
 			<div class="col-md-4 pull-right">
-				<h2 class="text-center">gagner des badge</h2>
+				<h2 class="text-center">gagnez des badge</h2>
 				<p class="text-center">
-					gagner des badge sur chaque defi réussi et montre au monde vous exploit
+					gagnez des badges sur chaque defi réussi et montrez au monde vos exploits
 				</p>
 			</div>
 			<div class="col-md-4">
-				<img class="img-responsive center-block" src="/img/badge.png">
+				<img class="img-responsive center-block" src="images/badge.png">
 			</div>
 			
 			
 		</div>		
 	</div>
 
-<?php
-//setAlert('success','hey..ho, we are on the way..check php/home.php to see how easy it is to use our alert system.');// this one is here to show the muscles of our alert system :D
-?>
+</body>
+</html>		
